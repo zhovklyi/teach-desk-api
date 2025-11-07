bfbc01be24ce01eed2002b087801d177a0dd0b2c
Zhovklyi Vladyslav <zhovklyi.vlad@gmail.com>
2025-11-07 11:52:30 +0200

    Login and register route rate limits added

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')
     ->group(function () {
-        Route::post('/login', [AuthController::class, 'login']);
-        Route::post('/register', [AuthController::class, 'register']);
+        Route::middleware(['throttle:5,1'])
+            ->group(function () {
+                Route::post('/login', [AuthController::class, 'login']);
+                Route::post('/register', [AuthController::class, 'register']);
+            });
 
         Route::middleware(['auth:sanctum'])
             ->group(function () {
